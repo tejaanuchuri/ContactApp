6e2f7d2412a7826f2687b92c83412eb977be74d4
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-07-22 12:08:20 +0530

    "we have created a  list of people"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = React.createElement('div',{className:'welcome message'},'hello world')
+const element = React.createElement('ol',{className:'welcome message'},
+ React.createElement('li',null,'Teja'),
+ React.createElement('li',null,'suresh'),
+ React.createElement('li',null,'jnani')
+)
 
 console.log(element)
 
